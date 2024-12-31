50648b22677a2269ef9cf979626b0b94212d1414
ridhi <ridhisriramula@gmail.com>
2024-12-31 06:27:17 +0530

    FirstCommit

---
+++ src/main/java/sejava/javaexample/App.java
@@ -0,0 +1,10 @@
+package sejava.javaexample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello maven java!");
+    }
+}
